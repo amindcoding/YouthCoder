123719de54b43f26e01f514b5cc6be9293acd3e2
amindcoding <muhammadaminrizki17@hotmail.com>
2022-02-05 21:36:55 +0700

    Commit Amin

--- website/src/App.js
+++ website/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Contact from './routes/Contact';
-
 import Home from './routes/Home';
 import Pricing from './routes/Pricing';
 import Training from './routes/Training';
@@ -10,10 +9,10 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/training" element={<Training />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/pricing" element={<Pricing />} />
+        <Route exact path="/training" element={<Training />} />
+        <Route exact path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
--- website/src/routes/Home.js
+++ website/src/routes/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import Video from '../components/Video';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
