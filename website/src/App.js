fe5c31c513019af67fecb9ecd9460933dd31d314
amindcoding <muhammadaminrizki17@hotmail.com>
2022-02-05 21:14:50 +0700

    Routing
    Tadi harus install react-router-dom@6.2.1

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Contact from './routes/Contact';
 
 import Home from './routes/Home';
@@ -8,14 +8,14 @@ import Training from './routes/Training';
 
 function App() {
   return (
-    <div>
-      <Route>
-        <Router exact path="/" element={<Home />} />
-        <Router exact path="/pricing" element={<Pricing />} />
-        <Router exact path="/training" element={<Training />} />
-        <Router exact path="/contact" element={<Contact />} />
-      </Route>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/training" element={<Training />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
