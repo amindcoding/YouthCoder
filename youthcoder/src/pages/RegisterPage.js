7654686fc835a7e022049152e8eab4c1081d388e
ai-nur <98943960+ai-nur@users.noreply.github.com>
2022-02-07 10:43:17 +0700

    register page

---
+++ youthcoder/src/pages/RegisterPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import RegisterComponent from '../components/RegisterComponent';
+
+export default class RegisterPage extends Component {
+  render() {
+    return (
+        <RegisterComponent />
+    );
+  }
+}
