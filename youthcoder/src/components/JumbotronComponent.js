9b7d429575c98c967dd339155ae3c7448d6115d5
amindcoding <muhammadaminrizki17@hotmail.com>
2022-02-08 09:11:49 +0700

    Update JumbotronComponent.js

--- youthcoder/src/components/JumbotronComponent.js
+++ youthcoder/src/components/JumbotronComponent.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { Container } from 'reactstrap';
 
 const JumbotronComponent = () => {
+  const userInfo = localStorage.getItem('userInfo');
+
   return (
     <div>
       <div
@@ -15,11 +17,17 @@ const JumbotronComponent = () => {
           <div class="container-fluid py-5 text-white">
             <h1 class="display-5 fw-bold">Youth Coder</h1>
             <p class="col-md-8 fs-4">Website pembelejaran untuk programmer pemula. Lingkungan untuk yang ingin belajar pemrograman dari awal.</p>
-            <Link to="/register">
-              <button class="btn btn-primary btn-lg" type="button">
-                Daftar Sekarang
-              </button>
-            </Link>
+            {userInfo ? (
+              <h2>
+                Welcome {JSON.parse(userInfo).firstName} {JSON.parse(userInfo).lastName}
+              </h2>
+            ) : (
+              <Link to="/register">
+                <button class="btn btn-primary btn-lg" type="button">
+                  Daftar Sekarang
+                </button>
+              </Link>
+            )}
           </div>
         </Container>
       </div>
