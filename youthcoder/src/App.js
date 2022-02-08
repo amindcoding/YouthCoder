a3c679ce8bdfdba07c090331cbde7c8324ca82ce
ai-nur <98943960+ai-nur@users.noreply.github.com>
2022-02-08 09:11:13 +0700

    profile page
    edit profil user

--- youthcoder/src/App.js
+++ youthcoder/src/App.js
@@ -1,16 +1,21 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import RegisterPage from './pages/RegisterPage';
-import LoginPage from './pages/LoginPage';
-import HomePage from './pages/Home';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/Home";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
+  const userInfo = localStorage.getItem("userInfo");
+  let id = JSON.parse(userInfo).id ;
+
   return (
     <>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/register" element={<RegisterPage />} />
         <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path={"/profile/" + id} element={<ProfilePage />} />
       </Routes>
     </>
   );
